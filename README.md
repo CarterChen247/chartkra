2babeacd5a349888d55553303083686e09ed032f
CarterChen247 <carterchen247@gmail.com>
2017-12-18 14:26:32 +0800

    refactor heatmap

--- README.md
+++ README.md
@@ -1,58 +1,119 @@
-# chartkra
+# Introduction
 
-Chartkra stands for `chart` and `charkra`. It facilitates the displaying from `matplotlib` to be embed in `PyQt` Widgets.
+    pip3 install chartkra
+    
+chartkra facilitates the chart displaying on PyQt widgets by the use of matplotlib. It can easily to display the following figures:
 
-## version 0.0.6
+1. plots
+2. bars
+3. pies
+4. heatmaps
 
-Only provides simple visualization features, such as plots, bars, pies and heatmaps.
+# Usage
 
-## usage
+It's very easy to getting started with chartkra. What you need to do is to instantiate a `chartkra` object, and pass its `get_widget` function to PyQt Widget's `addWidget` function:
 
-By using `chartkra`, it is very easy to draw `matplotlib`'s figure to `PyQt` Widget, the implementation codes will take no longer than 3 lines.
+    # initialize
+    chart = chartkra()
+    self.gridLayout.addWidget(chart.get_widget())
+    # draw charts
+    # ...
 
-### import
+It's the same way to showing a heatmap, just need to instantiate  a `HeatMap` object instead.
 
-````python
-from chartkra.chartkra import chartkra
-````
+    # initialize
+    heatmap = HeatMap()
+    self.gridLayout.addWidget(heatmap.get_widget())
+    # draw heatmap
+    # ...
 
-### draw plots
-````python
-plot = chartkra()
-self.gridLayout.addWidget(plot.get_widget())
-plot.draw_plot(listx=['12/01','12/02','12/03'], listy=[1,2,3])
-````
 
-### draw bars
-````python
-bar = chartkra()
-self.gridLayout.addWidget(bar.get_widget())
-bar.draw_bar(listx=['zone1', 'zone2', 'zone3'], listy=[1, 2, 3])
-````
+# Drawing Figures
+## Drawing Plots
 
-### draw pies
-````python
-pie = chartkra()
-self.gridLayout.addWidget(pie.get_widget())
-pie.draw_pie(sizes=[1, 2, 3], labels=['zone1', 'zone2', 'zone3'])
-````
+Use function `draw_plot` to draw. Pass what you're going to show on x axis to `listx`, and what you're going to show on y axis to `listy`.
 
-### draw heatmap
-````python
-heatmap = chartkra()
-points = [[2, 2], [4, 3], [4, 4], [4, 4], [4, 4], [4.1, 4.1]]
-self.gridLayout.addWidget(heatmap.get_widget(), 0, 1, 2, 1)
-heatmap.draw_heatmap(
-    points=points,
-    accuracy=10, 
-    zone_width=8.2, 
-    zone_height=10.836, 
-    zone_pic='office_inte.jpg', 
-    canvas_width=8, 
-    canvas_height=6, 
-    canvas_dpi=100
-)
-````
+    chart.draw_plot(listx=['12/01', '12/02', '12/03'], listy=[1, 2, 3])
+![](https://d2mxuefqeaa7sj.cloudfront.net/s_82697D89B2767AD0FD740BDF7DC3C2E953BC1C71AA09B2F6E6AD02DD94183EED_1513569828224_plot.png)
 
-### demo
-![](https://github.com/KazafChen/chartkra/blob/master/demo.png)
+## Drawing Bars
+
+Use function `draw_bar` to draw. Pass what you're going to show on x axis to `listx`, and what you're going to show on y axis to `listy`.
+
+    chart.draw_bar(listx=['zone1', 'zone2', 'zone3'], listy=[1, 2, 3])
+![](https://d2mxuefqeaa7sj.cloudfront.net/s_82697D89B2767AD0FD740BDF7DC3C2E953BC1C71AA09B2F6E6AD02DD94183EED_1513569861075_bar.png)
+
+## Drawing Pies
+
+Use function `draw_pie` to draw. The sizes and the labels reference the partition in the pie figure respectively.
+
+    chart.draw_pie(sizes=[1, 2, 3], labels=['zone1', 'zone2', 'zone3'])
+![](https://d2mxuefqeaa7sj.cloudfront.net/s_82697D89B2767AD0FD740BDF7DC3C2E953BC1C71AA09B2F6E6AD02DD94183EED_1513569878247_pie.png)
+
+## Drawing Heatmaps
+
+###Example:Pedestrian Trajectory Heatmap
+
+![](https://d2mxuefqeaa7sj.cloudfront.net/s_82697D89B2767AD0FD740BDF7DC3C2E953BC1C71AA09B2F6E6AD02DD94183EED_1513570063080_demo.png)
+
+
+###codes：
+
+    heatmap.set_zone(pic='office_inte.jpg', width=8.2, height=10.836, points=points)
+    heatmap.set_display(accuracy=25, style=plt.cm.jet, rotation=0, title='Pedestrian Trajectory Heatmap')
+    heatmap.set_colorbar(position='right', width='5%', padding=0.1)
+    heatmap.show()
+    
+When creating a heatmap, it is needed to declare the function `set_zone` and its arguments: 
+
+- pic: the path of picture which you want to show in heatmap.
+- width: width of the picture(zone).
+- height: height of the picture(zone).
+- points: the points scattered in the zone, the units of each point need to be the same as the width and the height of picture(zone).
+
+By setting up argument `title` and `title_padding` in function `set_display`, you can customized the title of figure and the padding between title and figure.
+
+###Accuracy
+
+![](https://d2mxuefqeaa7sj.cloudfront.net/s_82697D89B2767AD0FD740BDF7DC3C2E953BC1C71AA09B2F6E6AD02DD94183EED_1513571561142_hm_acc-01.png)
+
+
+It is customized by argument `accuracy` in function `set_display`
+
+###Styles
+
+![](https://d2mxuefqeaa7sj.cloudfront.net/s_82697D89B2767AD0FD740BDF7DC3C2E953BC1C71AA09B2F6E6AD02DD94183EED_1513570926332_hm_styles.png)
+
+
+It is customized by argument `style` in function `set_display`. The accepted styles can be found on https://matplotlib.org/examples/color/colormaps_reference.html
+
+###Orientations
+
+![](https://d2mxuefqeaa7sj.cloudfront.net/s_82697D89B2767AD0FD740BDF7DC3C2E953BC1C71AA09B2F6E6AD02DD94183EED_1513571466433_hm_rot-01.png)
+
+
+It is customized by argument `rotation` in function `set_display`. The accepted values includes 0, 90, 180 and 270.
+
+###Colorbars
+
+![](https://d2mxuefqeaa7sj.cloudfront.net/s_82697D89B2767AD0FD740BDF7DC3C2E953BC1C71AA09B2F6E6AD02DD94183EED_1513571763704_hm_colorb-01.png)
+
+It is customized by assign arguments in function `set_colorbar`
+
+- position: accepted values includes top, right, left, bottom.
+- width: the width of the colorbar.
+- padding: distance between colorbar and figure.
+
+###Others
+
+![](https://d2mxuefqeaa7sj.cloudfront.net/s_82697D89B2767AD0FD740BDF7DC3C2E953BC1C71AA09B2F6E6AD02DD94183EED_1513571814004_hm_display-01.png)
+
+- The showing of colorbars is not requires. The colorbars will not be showed by default if you don't declare function `set_colorbar`.
+- By using function `disable_heatmap_layer`, the figure will only display picture.
+- By using function `disable_img_layer`, the figure will only display heatmap layer.
+
+###Save Figure to File
+In general situations, using function `show` would be enough for showing a figure. If you want to save the figure to file as well, just declare function `show_and_save_fig` instead and assign arguments `filename` and display resolution like: 
+
+    heatmap.show_and_save_fig(filename='sample.png', dpi=150)
+
--- chartkra/heatmap.py
+++ chartkra/heatmap.py
@@ -1,91 +1,215 @@
+import pprint
+
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.colors import ListedColormap
+from matplotlib.figure import Figure
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
 import numpy as np
-from PIL import Image
 from matplotlib import pyplot as plt
+from scipy.misc import imread
+from scipy.ndimage import rotate, filters
 
-class heatMap:
-    def __init__(self, ax, points, accuracy, zone_width, zone_height, pic):
 
-        # load img
-        img = Image.open(pic)
+class HeatMap:
+    def __init__(self):
+        # pyqt objects
+        self.figure = Figure()
+        self.canvas = FigureCanvas(self.figure)
+        self.ax = self.figure.add_subplot(111)
 
-        origin = np.asarray(img)
-        height_px = origin.shape[0]
-        width_px = origin.shape[1]
+        # matplotlib objects
+        self.pic = None
+        self.zone_width = None
+        self.zone_height = None
+        self.points = None
+        self.accuracy = 25
+        self.style = plt.cm.jet
+        self.rotate_angle = 0
+        self.title = None
+        self.title_padding = 1
+        self.colorbar_enabled = False
+        self.colorbar_position = 'right'
+        self.colorbar_width = '5%'
+        self.colorbar_padding = 0.1
 
-        # resize
-        img = img.resize((width_px * 2, height_px * 2))
-        A = np.asarray(img)
+        # for calculation
+        self.ratio = None
 
-        # extent (need to redraw, axis will be reset)
-        extent = (0, width_px, height_px, 0)
-        ax.imshow(A, extent=extent)
+        # for displaying
+        self.hide_img_layer = False
+        self.hide_heatmap_layer = False
 
-        """
-        codes = [Path.MOVETO] + [Path.LINETO] * 3 + [Path.CLOSEPOLY]
-        vertices = [(100, 100), (100, 200), (200, 200), (200, 100), (0, 0)]
-        vertices = np.array(vertices, float)
-        path = Path(vertices, codes)
-        area_draw = PathPatch(path, facecolor='None', edgecolor='green')
-        ax.add_patch(area_draw)
-        """
+    def get_widget(self):
+        return self.canvas
+
+    def set_zone(self, pic, width, height, points):
+        self.pic = pic
+        self.zone_width = width
+        self.zone_height = height
+        self.points = points
+
+    def set_display(self, accuracy, style, rotation, title, title_padding=1):
+        self.accuracy = accuracy
+        self.style = style
+        self.rotate_angle = rotation
+        self.title = title
+        self.title_padding = title_padding
+
+    def set_colorbar(self, position, width, padding):
+        self.colorbar_enabled = True
+        self.colorbar_position = position
+        self.colorbar_width = width
+        self.colorbar_padding = padding
+
+    def disable_img_layer(self):
+        self.hide_img_layer = True
+
+    def disable_heatmap_layer(self):
+        self.hide_heatmap_layer = True
+
+    def show(self):
+
+        if self.pic is None or self.zone_width is None or self.zone_height is None or self.points is None:
+            raise RuntimeError('generating heatmap requires real-world values')
+
+        # load picture
+        origin_pic = imread(self.pic)
+
+        # get width and height
+        pic_height_px = origin_pic.shape[0]
+        pic_width_px = origin_pic.shape[1]
+
+        # store rotated picture
+        rotated_pic = rotate(origin_pic, self.rotate_angle)
+
+        # values for calculate extent (avoid distortion)
+        size = np.zeros((pic_height_px, pic_width_px))
+        size_rotated = rotate(size, self.rotate_angle)
+        extent_args = np.zeros((2, 2))
+        extent_args[0, 0] = 0
+        extent_args[0, 1] = size_rotated.shape[1]
+        extent_args[1, 0] = size_rotated.shape[0]
+        extent_args[1, 1] = 0
+
+        # define extent
+        extent = (extent_args[0, 0], extent_args[0, 1], extent_args[1, 0], extent_args[1, 1])
 
         # map meter to pixel
         # width_meter = width_px / ratio
         # width_px = width_meter * ratio
-        ratio = width_px / zone_width
+        self.ratio = pic_width_px / self.zone_width
 
-        # calculte grid size
+        # calculate grid size
         # use short size to calculate
-        short_px = width_px
-        long_px = height_px
-        if width_px > height_px:
-            short_px = height_px
-            long_px = width_px
+        short_px = pic_width_px
+        long_px = pic_height_px
+        if pic_width_px > pic_height_px:
+            short_px = pic_height_px
+            long_px = pic_width_px
 
         # calculate block size
-        part = short_px / accuracy
-        grid_size_short_f = accuracy
+        part = short_px / self.accuracy
+        grid_size_short_f = self.accuracy
         grid_size_long_f = long_px / part
 
         # generate coordinate at each axis
-        ticks_x = np.linspace(0, width_px, grid_size_short_f)
-        ticks_y = np.linspace(0, height_px, grid_size_long_f)
-        if width_px > height_px:
-            ticks_x = np.linspace(0, width_px, grid_size_long_f)
-            ticks_y = np.linspace(0, height_px, grid_size_short_f)
+        ticks_x = np.linspace(0, pic_width_px, grid_size_short_f)
+        ticks_y = np.linspace(0, pic_height_px, grid_size_long_f)
+        if pic_width_px > pic_height_px:
+            ticks_x = np.linspace(0, pic_width_px, grid_size_long_f)
+            ticks_y = np.linspace(0, pic_height_px, grid_size_short_f)
 
         # record score
-        score = np.zeros((round(grid_size_long_f), round(grid_size_short_f)))
+        score_row = round(grid_size_long_f)
+        score_col = round(grid_size_short_f)
+        if pic_width_px > pic_height_px:
+            score_row = round(grid_size_short_f)
+            score_col = round(grid_size_long_f)
 
-        def update_score(score, points_input):
-            points = np.array(points_input)
+        # create score table
+        scores = np.zeros((score_row, score_col))
 
-            for point in points:
-                point_px = point * ratio
+        # update score
+        self.update_score(scores, self.points, ticks_x, ticks_y)
 
+        # mapping heatmap and zone picture
+        scores = rotate(scores, self.rotate_angle)
 
-                # find index
+        # enlarge heat zones by gaussian blur
+        heat = filters.gaussian_filter(scores, 1)
 
-                idx = 0
-                dist = abs(point_px[0] - ticks_x[0])
-                for i in range(0, len(ticks_x)):
-                    value = abs(point_px[0] - ticks_x[i])
-                    if value < dist:
-                        dist = value
-                        idx = i
+        colormap = self.style
+        custom_colormap = colormap(np.arange(colormap.N))
+        custom_colormap[:, -1] = np.linspace(0, 1, colormap.N)
+        custom_colormap = ListedColormap(custom_colormap)
 
-                idy = 0
-                dist = abs(point_px[1] - ticks_y[0])
-                for i in range(0, len(ticks_y)):
-                    value = abs(point_px[1] - ticks_y[i])
-                    if value < dist:
-                        dist = value
-                        idy = i
+        max_score = np.amax(scores)
+        max_heat = np.amax(heat)
+        compensate = max_score / max_heat
+        heat_show = heat * compensate
 
-                print('index x,y=' + str(idx) + ',' + str(idy))
+        # the axis is difficult to handle with rotations, so hide it temporarily
+        self.ax.axis('off')
 
-                score[idy][idx] = score[idy][idx] + 1
+        if self.title is not None:
+            self.ax.set_title(self.title, y=self.title_padding)
 
-        update_score(score, points)
+        # show zone picture
+        if self.hide_img_layer is False:
+            self.ax.imshow(rotated_pic, extent=extent)
 
-        ax.imshow(score, cmap=plt.cm.coolwarm, alpha=.9, interpolation='bilinear', extent=extent)
+        if self.hide_heatmap_layer is False:
+
+            if self.hide_img_layer is False:
+                heatmap = self.ax.imshow(heat_show, extent=extent, interpolation='lanczos', cmap=custom_colormap)
+            else:
+                heatmap = self.ax.imshow(heat_show, extent=extent, interpolation='lanczos', cmap=self.style)
+
+            if self.colorbar_enabled is True:
+                self.show_colorbar(heatmap)
+
+    def update_score(self, score, points_input, ticks_x, ticks_y):
+        points = np.array(points_input)
+
+        for point in points:
+            point_px = point * self.ratio
+
+            # find index
+            idx = 0
+            dist = abs(point_px[0] - ticks_x[0])
+            for i in range(0, len(ticks_x)):
+                value = abs(point_px[0] - ticks_x[i])
+                if value < dist:
+                    dist = value
+                    idx = i
+
+            idy = 0
+            dist = abs(point_px[1] - ticks_y[0])
+            for i in range(0, len(ticks_y)):
+                value = abs(point_px[1] - ticks_y[i])
+                if value < dist:
+                    dist = value
+                    idy = i
+
+            # update score
+            score[idy][idx] = score[idy][idx] + 1
+
+    def show_colorbar(self, heatmap):
+
+        # locate
+        divider = make_axes_locatable(self.ax)
+        cax = divider.append_axes(self.colorbar_position, size=self.colorbar_width, pad=self.colorbar_padding)
+
+        # determine orientation
+        orientation = 'vertical'
+        if self.colorbar_position is 'right' or self.colorbar_position is 'left':
+            orientation = 'vertical'
+        elif self.colorbar_position is 'top' or self.colorbar_position is 'bottom':
+            orientation = 'horizontal'
+        # show colorbar
+        self.figure.colorbar(heatmap, orientation=orientation, cax=cax)
+
+    def show_and_save_fig(self, filename='chartkra_heatmap.png', dpi=150):
+        self.show()
+        self.figure.savefig(fname=filename, dpi=dpi)
